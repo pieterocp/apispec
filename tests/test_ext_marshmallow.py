0584105724aa74fced4c0b52cdd726c6880d62d0
Steven Loria <sloria1@gmail.com>
2025-05-12 10:56:08 -0400

    Update build matrix; address warnings in tests (#978)
    * Update build matrix
    
    * Address marshmallow 4 warnings in tests
    
    * Fix py39 test

--- tests/test_ext_marshmallow.py
+++ tests/test_ext_marshmallow.py
@@ -1,8 +1,18 @@
 import json
+import sys
 
 import pytest
 from marshmallow import Schema
-from marshmallow.fields import DateTime, Dict, Field, List, Nested, String, TimeDelta
+from marshmallow.fields import (
+    DateTime,
+    Dict,
+    Field,
+    Int,
+    List,
+    Nested,
+    String,
+    TimeDelta,
+)
 
 from apispec import APISpec
 from apispec.exceptions import APISpecError
@@ -12,7 +22,6 @@ from .schemas import (
     AnalysisSchema,
     AnalysisWithListSchema,
     DefaultValuesSchema,
-    OrderedSchema,
     PatternedObjectSchema,
     PetSchema,
     RunSchema,
@@ -1246,8 +1255,19 @@ class TestSelfReference:
         }
 
 
-class TestOrderedSchema:
-    def test_ordered_schema(self, spec):
+class TestFieldOrdering:
+    def test_field_order_preserved(self, spec):
+        class OrderedSchema(Schema):
+            field1 = Int()
+            field2 = Int()
+            field3 = Int()
+            field4 = Int()
+            field5 = Int()
+            if sys.version_info < (3, 10):
+
+                class Meta:
+                    ordered = True
+
         spec.components.schema("Ordered", schema=OrderedSchema)
         result = get_schemas(spec)["Ordered"]["properties"]
         assert list(result.keys()) == ["field1", "field2", "field3", "field4", "field5"]
